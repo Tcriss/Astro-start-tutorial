24cfcba106c4e189829edecbc5e4496f26a4e212
Cristian <do.cristian.tr@hotmail.com>
2024-07-25 10:25:55 -0400

    feat: ui frameworks components
    I manage to include components from Preact, and the ability to use others integrations too.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
-const site = import.meta.env.SITE
+const site = import.meta.env.SITE;
 
 // https://astro.build/config
-export default defineConfig({ site });
+export default defineConfig({
+  site,
+  integrations: [preact()]
+});
