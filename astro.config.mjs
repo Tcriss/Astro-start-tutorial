59198690ecb15f829818c2272c1a135170aef807
Cristian <do.cristian.tr@hotmail.com>
2024-07-25 09:32:48 -0400

    feat: rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
+const site = import.meta.env.SITE
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ site });
